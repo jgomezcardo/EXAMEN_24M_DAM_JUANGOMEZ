897a1fef9ab7105b688bc6781fed0239cb7c0a30
Ceinmark <Ceinmark@DESKTOP-J0PN6TT>
2021-03-24 11:25:15 +0100

    Version 1.0

---
+++ src/Ejercicio.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ejercicio {
+
+	public static void main (String args[]) {
+		
+		System.out.println("Buenos días Ceinmark, soy Juan Gómez\r\n" + 
+				"\r\n" + 
+				"Introduce un número para averiguar si es primo o no:\r\n" + 
+				"");
+		
+	}
+}
